0fcf8592384002691275a39380dbf66341a6fd86
James Fisher <jfisher396@hotmail.com>
2020-06-30 16:04:06 -0700

    added cards for forecast

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 //TODO:create list for past search items
-//TODO:create card for current weather conditions
-//TODO:create cards for 5-day forcast
 //TODO:link UV index
 //TODO:get icons to show
 //TODO:setup local storage
@@ -10,15 +8,23 @@
 //Pulls the current date
 let NowMoment = moment().format("l");
 let displayDate = document.getElementById('currentDay');
+let day1 = moment().add(1, 'days').format('l');
+let day2 = moment().add(2, 'days').format('l');
+let day3 = moment().add(3, 'days').format('l');
+let day4 = moment().add(4, 'days').format('l');
+let day5 = moment().add(5, 'days').format('l');
+
 
 // searches the API for the chosen city
 $("#search-button").on("click", function(event) {
     event.preventDefault();
     var citySelection = $("#city-input").val();
+   
     console.log(citySelection)
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySelection + "&units=imperial&appid=42d98d76405f5b8038f2ad71187af430";
     console.log(queryURL);
-    var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySelection + "&units=imperial&appid=42d98d76405f5b8038f2ad71187af430";
+    // var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySelection + "&units=imperial&appid=42d98d76405f5b8038f2ad71187af430";
+    // var queryURL3 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly" + "&units=imperial&appid=42d98d76405f5b8038f2ad71187af430";
     let cities = document.querySelector("#city-input").value;
     console.log(cities);
     localStorage.setItem("cities", JSON.stringify(cities));
@@ -36,32 +42,30 @@ $("#search-button").on("click", function(event) {
         let cityWind = response.wind.speed;
         $("#city-name").text(cityName + " " + "(" + NowMoment + ")");
         $("#city-cond").text("Current Conditions: " + cityCond);
-        $("#temp").text("Temperature (F): " + cityTemp.toFixed(2));
+        $("#temp").text("Temperature (F): " + cityTemp.toFixed(1));
         $("#humidity").text("Humidity: " + cityHum + "%");
         $("#wind-speed").text("Wind Speed: " + cityWind + "mph");
-        
+        $("#date1").text(day1);
+        $("#date2").text(day2);
+        $("#date3").text(day3);
+        $("#date4").text(day4);
+        $("#date5").text(day5);
     });
     
     // $.ajax({
-    //     url: queryURL2,
+    //     url: queryURL3,
     //     method: "GET"
     // }).then(function (response) {
     //     console.log(response);
     //     console.log(response.list.length);
-    // for (let i = 0; i< response.list.length; i++) {
-    //     let forecastDiv = $("<div>");
-    //     forecastDiv.addClass("card").text("Forecast");
+    // for (let i = 0; i < response.list.length; i++) {
         
-    //     if (response.list[i].dt_txt.includes("15:00:00")) {
-    //         console.log($(this));
-    //     }
-        
-    //     $("#forecast").append(forecastDiv);
-    // }
-        
+    // })
+    
+    var pastCities = JSON.parse(localStorage.getItem("cities"));
+    console.log(pastCities)
+    $("#cityList").prepend("<td>" + pastCities + "</td>");
 
-    // });
-    // var pastCities = JSON.parse(localStorage.getItem("cities"));
-    // console.log(pastCities)
-    // $("#cityList").val("<tr></tr>");
-})
+    })
+    
+
